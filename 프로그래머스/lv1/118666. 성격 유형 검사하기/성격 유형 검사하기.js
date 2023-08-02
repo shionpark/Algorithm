60eb9c115ca99bd724db9b690c5088e0ab62fd72
Shion <79674766+shionpark@users.noreply.github.com>
2023-08-02 16:04:09 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,21 @@
+function solution(survey, choices) {
+    const result = {
+        "R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0,
+    }
+    
+    for (let i = 0; i < choices.length; i++) {
+        const score = choices[i] - 4;
+        const [left, right] = survey[i]
+        if (score !== 0) {
+            score < 0 ? result[left] += -score : result[right] += score;
+        }
+    }
+    // console.log(result);
+    
+    let answer = '';
+    answer += result["R"] >= result["T"] ? "R" : "T";
+    answer += result["C"] >= result["F"] ? "C" : "F";
+    answer += result["J"] >= result["M"] ? "J" : "M";
+    answer += result["A"] >= result["N"] ? "A" : "N";
+    return answer;
+}
