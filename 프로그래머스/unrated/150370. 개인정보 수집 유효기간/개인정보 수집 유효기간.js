4797c9a0ddb1b194b8d392bc2ad067d9ef83ab47
Shion <79674766+shionpark@users.noreply.github.com>
2023-08-06 23:43:08 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.60 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,30 +1,38 @@
 function solution(today, terms, privacies) {
+	// 1. today 연/월/일 분해하고 숫자로
+	const [todayYear, todayMonth, todayDay] = today.split(".").map(Number);
+    
     const answer = [];
-	const [todayYear, todayMonth, todayDay] = today.split(".").map(Number); 
-        
+    
+    // 2. terms = ["Z 3", "D 5"] 객체로 만들자
     const termsMap = {};
     terms.forEach(item => {
         const [data, termsMonth] = item.split(" ");
         termsMap[data] = Number(termsMonth);
     })
-        
-    privacies.forEach((item, idx) => { 
+    
+    console.log(termsMap) // termsMap = { Z: 3, D: 5 }
+    
+    // 3. privacies 배열에 termsMap의 key가 있다. (value)
+    privacies.forEach((item, idx) => {
         const [date, value] = item.split(" ");
-        const [year, month, day] = date.split(".").map(Number);
-        		
-        let endMonth = month + termsMap[value]; 
-        let endYear = year;
-        		
-        if (endMonth > 12) {
-          endYear += Math.floor((endMonth -1) / 12); 
-          endMonth = ((endMonth -1) % 12) + 1; 
+        const [year, month, day] = date.split(".").map(Number); // 개인정보 수집 날짜에
+        
+		// 개인정보 수집 기간 + 약관 기간 = 보관 가능 기한
+        let newMonth = month + termsMap[value]; 
+        let newYear = year;
+        
+		// 12개월 넘으면 1년 추가
+        if (newMonth > 12) {
+          newYear += Math.floor(newMonth / 12); // 월을 12로 나눈 몫만큼 연도에 추가
+          newMonth = newMonth % 12 + 1; // 월을 12로 나눈 나머지만 남김
         }
 
-        const todayTotalDays = todayYear * 12 * 28 + todayMonth * 28 + todayDay;
-        const endTotalDays = endYear * 12 * 28 + endMonth * 28 + day;
+        console.log(`보관 가능 기한은 ${idx} : ${newYear}.${newMonth}.${day}`);
         
-        if (endTotalDays <= todayTotalDays) {
-            answer.push(idx + 1); 
+        // 오늘 날짜와 비교한다. 보관가능일자 지난 경우 -> 파기
+        if ((newYear > todayYear) || (newYear === todayYear && newMonth > todayMonth) || (newYear === todayYear && newMonth === todayMonth && day > todayDay)) {
+            answer.push(idx);
         }
     });
     
