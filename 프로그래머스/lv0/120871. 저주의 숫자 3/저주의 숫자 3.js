536d5e0629cbe9878c525bf7a370934543ca81d9
Shion <79674766+shionpark@users.noreply.github.com>
2023-08-13 21:32:55 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,3 +1,5 @@
+// https://velog.io/@bakkomcoder/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%A0%80%EC%A3%BC%EC%9D%98-%EC%88%AB%EC%9E%90-3
+
 function solution(n) {
     const arr = Array.from({ length: 200 }, (value, index) => index) 
     const filtered = arr.filter(item => (
