6e8ab990199120c02cd0ec953a8317517ec12c59
Shion <79674766+shionpark@users.noreply.github.com>
2023-08-13 20:59:30 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,3 +1,5 @@
+// https://velog.io/@bakkomcoder/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%A0%84%EA%B5%AD-%EB%8C%80%ED%9A%8C-%EC%84%A0%EB%B0%9C-%EA%B3%A0%EC%82%AC
+
 function solution(rank, attendance) {
     let rankMap = {}
     let rankArr = []
