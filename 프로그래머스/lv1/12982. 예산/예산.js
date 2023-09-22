3e68c0566f4f45d7ce1de909e392d092cb3e65c9
Shion <79674766+shionpark@users.noreply.github.com>
2023-09-22 11:00:19 +0900

    [level 1] Title: 예산, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,15 +1,15 @@
+// https://velog.io/@bakkomcoder/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%98%88%EC%82%B0
+
 function solution(d, budget) {
     let count = 0
     let curSum = 0
     let curIndex = 0
     
-    d = d.sort((a, b) => a - b)
-    
-    for (let i = 0; i < d.length; i++) {
-        curSum += d[i]
-        count++
-        if (curSum === budget) { return count }
-        if (curSum > budget) { return count - 1 }
-    }
+    d.sort((a, b) => a - b).forEach(i=> {
+        curSum += i
+        if (curSum <= budget) {
+            count++
+        }
+    })
     return count;
 }
