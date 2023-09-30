b5c5b416fd6d876c9a8bf60c487283ceeac5852c
Shion <79674766+shionpark@users.noreply.github.com>
2023-09-30 11:56:39 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,12 +1,11 @@
 function solution(numbers) {
     
-    const strList = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    const number = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
-    for (let i = 0; i < strList.length; i++) {
-        if (numbers.includes(strList[i])) {
-            numbers = numbers.replaceAll(strList[i], i)
-        }
+    for (let i = 0; i < number.length; i++) { 
+        numbers = numbers.split(number[i]).join(i)
     }
+    
     const answer = Number(numbers)
     return answer
 }
