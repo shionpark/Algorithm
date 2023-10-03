6a6cf10fdec71f009731885905200a8e39449586
Shion <79674766+shionpark@users.noreply.github.com>
2023-10-03 13:48:39 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,20 +1,16 @@
 function solution(my_string) {
-    let arr = new Array(52).fill(0)
+    const CHAR_CODE_START = 65
+    const CHAR_CODE_SIZE = 25
+    const NOT_CHAR_CODE_START = CHAR_CODE_START + CHAR_CODE_SIZE + 1
+    const NOT_CHAR_CODE_SIZE = 6
     
-    let alphabetMap = {}
-    for (let charCode = 65; charCode <= 90; charCode++) {
-        let char = String.fromCharCode(charCode) 
-        alphabetMap[char] = charCode - 65
-    }
-    for (let charCode = 97; charCode <= 122; charCode++) {
-        let char = String.fromCharCode(charCode) 
-        alphabetMap[char] = charCode - (65 + 6)
+    let arr = new Array(52 + NOT_CHAR_CODE_SIZE).fill(0)
+    
+    for (let char of my_string) {
+        arr[char.charCodeAt() - CHAR_CODE_START]++
     }
     
-    for (let i = 0; i < my_string.length; i++) {
-        let index = alphabetMap[my_string[i]]
-        arr[index]++
-    }
+    arr.splice(NOT_CHAR_CODE_START - CHAR_CODE_START, NOT_CHAR_CODE_SIZE)
     
     return arr
 }
