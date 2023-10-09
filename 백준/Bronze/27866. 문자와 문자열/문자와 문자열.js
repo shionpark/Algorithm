b6c93b30121321b43097e9f7b251d06167d8d2ca
Shion <79674766+shionpark@users.noreply.github.com>
2023-10-10 00:21:45 +0900

    [Bronze V] Title: 문자와 문자열, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -3,6 +3,6 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const word = input[0];
-const idx = Number(input[1]);
+const idx = Number(input[1] - 1);
 
-console.log(word[idx - 1]);
+console.log(word[idx]);
