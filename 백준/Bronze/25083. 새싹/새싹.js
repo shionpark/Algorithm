bbdf482338e2bdd5d985e68f6fadc594906b9eb5
Shion <79674766+shionpark@users.noreply.github.com>
2023-10-10 13:49:21 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 9308 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+console.log(`         ,r'"7
+r\`-_   ,'  ,/
+ \\. ". L_r'
+   \`~\\/
+      |
+      |`);
