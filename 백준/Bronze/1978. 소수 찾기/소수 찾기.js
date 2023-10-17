50dd5e6a3433fe641561cf4d2b27baf7e9b82ef7
Shion <79674766+shionpark@users.noreply.github.com>
2023-10-18 00:39:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.js
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.js
@@ -1,19 +1,19 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const nums = input[1].split(" ").map(Number);
+
 const isPrime = (n) => {
     if (n == 1) {
       return false;
     }
     
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-      if (n % i === 0) {
-        return false;
-      }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
     }
+  }
+  return true;
+};
 
-    return true;
-}
-
-const input = require('fs').readFileSync('/dev/stdin').toString().trim();
-
-const [c, nums] = input.split("\n");
-
-console.log(nums.split(" ").filter(v => isPrime(v)).length);
+console.log(nums.filter((num) => isPrime(num)).length);
