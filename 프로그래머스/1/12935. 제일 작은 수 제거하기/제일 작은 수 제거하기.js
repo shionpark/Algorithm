ce3d26c4f593050e62d73cf4a600777cd36b58b4
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-05 00:10:39 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,5 +1,4 @@
 function solution(arr) {
-    let emptyArr = [-1]
     let filteredArr = arr.filter(i => i !== Math.min(...arr))
-    return (filteredArr.length === 0) ? emptyArr : filteredArr;
+    return (filteredArr.length === 0) ? [-1] : filteredArr;
 }
