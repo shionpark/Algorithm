8f9f9fa04a507d7f24a0ef70ab267bf0c90750a9
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-05 23:55:24 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,18 @@
+function solution(clothes) {
+    const clothesMap = {};
+    let answer = 1;
+    clothes.forEach(arr => {  
+        const [type, name] = arr;
+        if(clothesMap.hasOwnProperty(name)) {
+            clothesMap[name]++;    
+        }
+        else {
+            clothesMap[name] = 1; 
+        }
+    })
+
+    for(const key in clothesMap) {
+        answer *= (clothesMap[key] + 1);
+    }
+    return answer - 1;
+}
