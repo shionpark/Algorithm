a71b23d35451597ddf4634b80651ca22ab385b09
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-10 08:56:05 +0900

    [level 3] Title: 베스트앨범, Time: 0.16 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,23 +1,4 @@
 function solution(genres, plays) {
-    // 각 장르별 노래 재생횟수 합산
-    const genrePlayCount = genres.reduce((acc, genre, idx) => {
-        if (!acc[genre]) {
-            acc[genre] = { totalPlay: 0, songs: [] }
-        }
-        acc[genre].totalPlay += plays[idx]
-        acc[genre].songs.push({ index:idx, play: plays[idx] })
-        return acc;
-    }, {})
-
-    // 재생 횟수 많은 순으로 정렬
-    const sortedGenres = Object.keys(genrePlayCount).sort((a, b) => genrePlayCount[b].totalPlay - genrePlayCount[a].totalPlay);
-    
-    // 각 장르 내에서 재생 횟수가 많은 노래 순으로 정렬, 최대 2개씩
-    const result = []
-    sortedGenres.forEach((genre) => {
-        const sortedSongs = genrePlayCount[genre].songs.sort((a, b) => (b.play !== a.play) ? b.play - a.play : a.index - b.index);
-        result.push(sortedSongs[0].index);
-        if (sortedSongs.length > 1) result.push(sortedSongs[1].index)        
-    })
-    return result
+    var answer = [];
+    return answer;
 }
