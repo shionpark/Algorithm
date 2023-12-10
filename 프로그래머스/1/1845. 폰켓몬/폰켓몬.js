99095e432e6c1e641a726d7c8ac4ca7d618d4691
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-10 09:16:22 +0900

    [level 1] Title: 폰켓몬, Time: 0.25 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,6 @@
 function solution(nums) {
-    let mySet = [...new Set(nums)] // 중복 제거
+    let mySet = new Set(nums) // 중복 제거
     let max = nums.length / 2
     
-    return mySet.length > max ? max : mySet.length
+    return mySet.size > max ? max : mySet.size
 }
