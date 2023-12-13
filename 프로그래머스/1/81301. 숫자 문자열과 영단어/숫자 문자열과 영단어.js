481778149caf1bb3e7774af8aec7442902cd744f
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-13 12:42:37 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,12 +1,11 @@
 function solution(s) {
-    let answer = ""
     const words_list = "zero one two three four five six seven eight nine";
-    
     let words = words_list.split(" ");
     
     for (let i = 0; i < words.length; i++) {
-        const regex = new RegExp(words[i], 'g');
-        s = s.replace(regex, i);
+        while (s.includes(words[i])) {
+            s = s.replace(words[i], i);
+        }
     }
     
     return Number(s);
