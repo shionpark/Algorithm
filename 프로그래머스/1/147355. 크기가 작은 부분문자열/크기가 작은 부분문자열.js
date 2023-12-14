267c4a004afdc3ea52f2e0c4e6c1545fe922e719
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-15 08:24:07 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,10 @@
+function solution(t, p) {
+    let count = 0;
+    for (let i = 0; i <= t.length - p.length; i++) {
+        const sliced = t.slice(i, i + p.length)
+        if (+sliced <= +p) {
+            count++
+        }
+    }
+    return count
+}
