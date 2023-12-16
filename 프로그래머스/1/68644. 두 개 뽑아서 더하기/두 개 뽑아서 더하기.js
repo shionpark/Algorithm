fb5e8963c8ca29996799eb2fea90f3a718f58d2d
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-16 09:44:04 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.48 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    let answer = [];
+    
+    for (let i = 0; i < numbers.length - 1; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            answer.push(numbers[i] + numbers[j])
+        }
+    }
+    
+    let result = [...new Set(answer)]
+    result.sort((a, b) => a - b)
+    return result;
+}
