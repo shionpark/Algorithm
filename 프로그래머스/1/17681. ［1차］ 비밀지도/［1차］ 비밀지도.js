fe8b36656405c9aa3be92641cf1d881581af89e5
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-18 00:11:28 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,13 +1,14 @@
 function solution(n, arr1, arr2) {
+    let arr3 = [];
     let answer = [];
+
     for (let i = 0; i < n; i++) {
-        answer.push(
-            (arr1[i] | arr2[i])
-            .toString(2)
-            .padStart(n, 0)
-            .replace(/1/gi, "#")
-            .replace(/0/gi, " ")
-        );
-    }    
-    return answer;
+        // 비트 OR연산자는 각 비트에 대해 둘 중 하나라도 1이면 결과를 1로 설정함
+        arr3.push(arr1[i] | arr2[i]);
+    }
+    
+    arr3.map(el => {
+        answer.push(el.toString(2).padStart(n, 0).replace(/1/gi, "#").replace(/0/gi, " "))
+    })
+    return answer
 }
