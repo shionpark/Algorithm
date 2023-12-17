2a743dc335b7bfb5c16d8bcd90649d11e6e81419
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-17 23:59:42 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,30 +1,13 @@
 function solution(n, arr1, arr2) {
-    let stk = [];
-    arr1.map(el => {
-        stk.push(el.toString(2).padStart(n, 0))
-    })
-    arr2.map(el => {
-        stk.push(el.toString(2).padStart(n, 0))
-    })
-    
-    let result = ""
-    let answer = ""
-    
-    for (let i = 0; i <n; i++) {
-        for (let j = 0; j < n; j++) {
-            if ((stk[i][j] === "1") || (stk[i + n][j] === "1")) {
-                result += "#"
-            } else {
-                result += " "
-            }
-        }
-    }
-    answer += result
-    
-    let slicedStrings = []
-    for (let i = 0; i < answer.length; i += n) {
-      let slicedPart = answer.slice(i, i + n);
-      slicedStrings.push(slicedPart);
-    }
-    return slicedStrings
+    let answer = [];
+    for (let i = 0; i < n; i++) {
+        answer.push(
+            (arr1[i] | arr2[i])
+            .toString(2)
+            .padStart(n, 0)
+            .replace(/1/gi, "#")
+            .replace(/0/gi, " ")
+        );
+    }    
+    return answer;
 }
