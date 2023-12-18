6194b85cdd788659e8682b9bdcf6b5a0ecd97fb7
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-18 23:52:37 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -2,10 +2,8 @@ function solution(a, b, n) {
     let getBottles = 0
     
     while (n >= a) {
-        let remainBottles = n % a
-        console.log("소유 : ", remainBottles)
-        n = Math.floor(n / a) * b
-        console.log("획득 : ", n)
+        let remainBottles = n % a // 팔기 전 남은 병 수
+        n = Math.floor(n / a) * b // 획득한 병 수
         getBottles += n
         n += remainBottles
     }
