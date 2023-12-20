7c6fe1e0b34219039c883a03af2b77cd877aeb23
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-20 10:28:32 +0900

    [level 1] Title: 2016년, Time: 0.27 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,14 +1,4 @@
 function solution(a, b) {
-    let answer = 0
-    
-    // 2016년 1월 1일은 금요일
-    const month = ["FRI","SAT","SUN","MON","TUE","WED","THU"]
-    const days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 30]
-    
-    for (let i = 0; i < a - 1; i++) {
-        answer += days[i]
-    }
-    answer += b - 1
-    
-    return month[answer % 7];
+    const month = a - 1
+    return new Date(2016, month, b).toString().slice(0, 3).toUpperCase()
 }
