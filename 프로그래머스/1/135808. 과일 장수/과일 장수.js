c88d462fbbc70d56aa87268b4c21b55ecde61f90
Shion <79674766+shionpark@users.noreply.github.com>
2023-12-22 09:12:01 +0900

    [level 1] Title: 과일 장수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,11 +1,11 @@
 function solution(k, m, score) {
-    const cnt = []
-    var li = score.sort((a,b) => b-a);
-   
-    let sum =0
+    let sum = 0
     
-    for(let i = 0; i+m <= li.length; i+=m){
-        sum+= li[i+m-1]*m
+    score.sort((a, b) => b - a);
+    
+    for (let i = 0; i + m <= score.length; i += m) {
+        let min = score[i + m - 1] * m
+        sum += min
     }
 
     return sum;
