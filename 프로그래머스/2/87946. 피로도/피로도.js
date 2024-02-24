dada9e46464fe3833f9202e66fb33938957a7c31
Shion <79674766+shionpark@users.noreply.github.com>
2024-02-24 18:58:38 +0900

    [level 2] Title: 피로도, Time: 0.89 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,38 +1,37 @@
 function solution(k, dungeons) {
-    let answer = -1;
+    let answer = 0;
     
-    const getPermutations = (arr, num) => {
-        if (num === 1) return arr.map((value) => [value]);
-
-        const results = [];
+    const getPermutations = (arr, selectNum) => {
+        const results = []
+        if (selectNum === 1) return arr.map((value) => [value]);
         
         arr.forEach((fixed, index, origin) => {
-            const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-            const permutaions = getPermutations(rest, num - 1);
-            const attached = permutaions.map((value) => [fixed, ...value]);
-            results.push(...attached);
-        });
-
-        return results;
+            const rest = [...origin.slice(0, index), ...origin.slice(index + 1)]
+            const combinations = getPermutations(rest, selectNum - 1)
+            const attached = combinations.map((combination) => [fixed, ...combination])
+            results.push(...attached)
+        })     
+        return results
     };
-
-  const permutaions = getPermutations(dungeons, dungeons.length);
     
-  for (let i = 0; i < permutaions.length; i++) {
-    const dungeon = permutaions[i];
-
-    let count = 0;
-    let hp = k;
-    for (let j = 0; j < dungeon.length; j++) {
-      const [needHp, useHp] = dungeon[j];
-      if (hp >= needHp) {
-        count++;
-        hp -= useHp;
-      }
+    const permutaions = getPermutations(dungeons, dungeons.length);
+    
+    for (let i = 0; i < permutaions.length; i++) {
+        const curPath = permutaions[i]
+        let curHp = k;
+        let count = 0;
+        
+        for (let j = 0; j < curPath.length; j++) {
+             const curDungeon = curPath[j]
+             const [minHp, useHp] = curDungeon
+             
+             if (curHp >= minHp) {
+                 count++
+                 curHp -= useHp
+             }
+        }
+        answer = Math.max(answer, count)
     }
-    answer = Math.max(answer, count);
-  }
-
-  return answer;
+    return answer;
 }
 
