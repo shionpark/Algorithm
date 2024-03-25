c77ef500d2fabab1011e53280471a163fe5cd761
Shion <79674766+shionpark@users.noreply.github.com>
2024-03-25 21:50:49 +0900

    [Bronze II] Title: 진법 변환, Time: 192 ms, Memory: 9640 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split(" ");
+
+const [N, B] = input;
+
+console.log(parseInt(N, Number(B)));
