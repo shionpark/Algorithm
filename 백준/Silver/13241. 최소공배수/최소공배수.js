b0437c0741888e8a4d79d3cc17fb5e4fbecf3edd
Shion <79674766+shionpark@users.noreply.github.com>
2024-05-12 08:47:59 +0900

    [Silver V] Title: 최소공배수, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const getGcd = (num1, num2) => {
+  let gcd = 1;
+
+  for (let i = 2; i <= Math.min(num1, num2); i++) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      gcd = i;
+    }
+  }
+
+  return gcd;
+};
+
+input.map((i) => {
+  const [num1, num2] = i.split(" ");
+  const gcd = getGcd(num1, num2);
+  console.log((num1 * num2) / gcd);
+});
