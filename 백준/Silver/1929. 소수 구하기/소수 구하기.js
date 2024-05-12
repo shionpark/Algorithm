faa4a6e42e022ed693cdb841f474a2db33c06b9c
Shion <79674766+shionpark@users.noreply.github.com>
2024-05-12 09:55:08 +0900

    [Silver III] Title: 소수 구하기, Time: 524 ms, Memory: 17220 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+let answer = [];
+
+const [min, max] = input;
+
+for (let i = min; i <= max; i++) {
+  if (isPrime(i)) answer.push(i);
+}
+
+console.log(answer.join("\n"));
