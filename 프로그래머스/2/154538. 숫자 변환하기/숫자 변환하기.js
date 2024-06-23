18e713154520edcee955361b123fe5640f877f24
Shion <79674766+shionpark@users.noreply.github.com>
2024-06-24 08:58:14 +0900

    [level 2] Title: 숫자 변환하기, Time: 31.10 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,13 +1,25 @@
 function solution(x, y, n) {
-    let dp = [...Array(y + 1).fill(Infinity)] 
+    let memo = new Array(y + 1).fill(1000001) // 초기값을 큰 값으로 설정
     
-    dp[x] = 0; 
+    memo[x] = 0; // 시작점 x에서의 연산 횟수는 0
     
-    for (let i = x; i <= y; i++) {
-        if (i + n <= y) dp[i + n] = Math.min(dp[i + n], dp[i] + 1)
-        if (i * 2 <= y) dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1)
-        if (i * 3 <= y) dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1)
+    for (let val = x; val <= y; val++) {
+        if (val - n >= x) {
+            memo[val] = Math.min(memo[val], memo[val - n] + 1)
+            console.log("1 ", val, memo)
+        }
+        
+        if (val % 2 === 0 && val / 2 >= x) {
+            memo[val] = Math.min(memo[val], memo[val / 2] + 1)
+            console.log("2 ", val, memo)
+        }
+        
+        if (val % 3 === 0 && val / 3 >= x) {
+            memo[val] = Math.min(memo[val], memo[val / 3] + 1)
+            console.log("3 ", val, memo)
+        }
+        
     }
     
-    return dp[y] === Infinity ? -1 : dp[y]
+    return memo[y] === 1000001 ? -1 : memo[y]
 }
